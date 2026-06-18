36c619607fe0c968e33dd7c141f775f7c9523da0
Eshan Chourasia <chourasiaeshan@gmail.com>
2026-06-19 00:07:31 +0530

    hotel booking api

---
+++ src/main/java/com/hotelManagement/projects/airBnbApp/AppUtils.java
@@ -0,0 +1,11 @@
+package com.hotelManagement.projects.airBnbApp;
+
+import com.codingshuttle.projects.airBnbApp.entity.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AppUtils {
+
+    public static User getCurrentUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
---
+++ src/main/java/com/hotelManagement/projects/airBnbApp/service/CheckoutService.java
@@ -0,0 +1,9 @@
+package com.hotelManagement.projects.airBnbApp.service;
+
+import com.codingshuttle.projects.airBnbApp.entity.Booking;
+
+public interface CheckoutService {
+
+    String getCheckoutSession(Booking booking, String successUrl, String failureUrl);
+
+}
